0c23934cf3c9deaeffb18d73439a7e2fe996659b
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-01-13 22:35:06 +0500

    added empty lines after gets

--- app3.rb
+++ app3.rb
@@ -1,26 +1,38 @@
 puts "Say a note on a 0 fret?" # Подразумевается, что мы скажем E
 gets
+
 puts "Say a note on a 1st fret?" # Подразумевается, что мы скажем F
 gets
+
 puts "Say a note on a 2nd fret?" # На втором ладу находится нота F#
 gets
+
 puts "Say a note on a 3rd fret?" # G
 gets
+
 puts "Say a note on a 4th fret?" # G#
 gets
+
 puts "Say a note on a 5th fret?" # A
 gets
+
 puts "Say a note on a 6th fret?" # A#
 gets
+
 puts "Say a note on a 7th fret?" # B
 gets
+
 puts "Say a note on a 8th fret?" # C
 gets
+
 puts "Say a note on a 9th fret?" # C#
 gets
+
 puts "Say a note on a 10th fret?" # D
 gets
+
 puts "Say a note on a 11th fret?" # D#
 gets
+
 puts "Say a note on a 12th fret?" # E
 gets
