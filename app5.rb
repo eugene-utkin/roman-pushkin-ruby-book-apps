316f1f0d94d587120f80931ecbca6f72b6122fb2
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-01-14 13:38:04 +0500

    puts/gets task with three clients

---
+++ app5.rb
@@ -0,0 +1,39 @@
+puts "First client date of birth?"
+first_birth = gets
+puts "First client place of birth?"
+first_place = gets
+puts "First client phone number?"
+first_number = gets
+
+puts "Second client date of birth?"
+second_birth = gets
+puts "Second client place of birth?"
+second_place = gets
+puts "Second client phone number?"
+second_number = gets
+
+puts "Third client date of birth?"
+third_birth = gets
+puts "Third client place of birth?"
+third_place = gets
+puts "Third client phone number?"
+third_number = gets
+
+puts "First client:"
+puts first_birth
+puts first_place
+puts first_number
+
+puts
+
+puts "Second client:"
+puts second_birth
+puts second_place
+puts second_number
+
+puts
+
+puts "Third client:"
+puts third_birth
+puts third_place
+puts third_number
