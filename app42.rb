6900cbb5064e45c4ffcaefa92cd35ff5c190094f
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-02-10 13:09:54 +0500

    simple method example

---
+++ app42.rb
@@ -0,0 +1,8 @@
+def get_number(what)
+    print "Введите #{what}: "
+    gets.to_i
+end
+
+age = get_number('возраст')
+salary = get_number('зарплату')
+rockets = get_number('количество ракет для запуска')
