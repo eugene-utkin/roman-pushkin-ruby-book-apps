3fe2effa53dca5b463c17dcbf8eea6dc19550461
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-03-02 23:54:08 +0500

    homework done

--- app51.rb
+++ app51.rb
@@ -1,13 +1,13 @@
-@arr1 = Array.new(10, 1)
-@arr2 = Array.new(10, 1)
+@arr1 = Array.new(10, 100)
+@arr2 = Array.new(10, 100)
 
 def check_stats
-  survivors1 = @arr1.count { |x| x == 1 }
-  survivors2 = @arr2.count { |x| x == 1 }
+  survivors1 = @arr1.count { |x| x > 0 }
+  survivors2 = @arr2.count { |x| x > 0 }
 
   puts "Robots left in first team: #{survivors1}"
 
-  random_sleep
+  time_to_sleep
 
   puts "Robots left in second team: #{survivors2}"
 
@@ -20,37 +20,62 @@ def check_stats
   end
 end
 
-def random_sleep
-  sleep rand(0.3..1.5)
+def time_to_sleep
+  sleep(1)
 end
 
 loop do
     check_stats
 
-    random_sleep
+    time_to_sleep
   
     puts 'First team attacks!'
-    random_sleep
+    time_to_sleep
     dice = rand(0..9)
-    if @arr2[dice] == 1
-      puts "Robot with index #{dice} is destroyed."
-      @arr2[dice] = 0
+    if @arr2[dice] > 0
+      damage = rand(30..100)
+      @arr2[dice] -= damage
+      puts "Robot with index #{dice} was hit for #{damage} damage points."
+      time_to_sleep
+      if @arr2[dice] > 0
+        puts "His health is reduced to #{@arr2[dice]} points."
+      else
+        puts "Robot with index #{dice} is destroyed."
+      end
     elsif @arr2[dice] == 0
       puts "Miss!"
     end
 
-    random_sleep
+    time_to_sleep
 
     puts 'Second team attacks!'
-    random_sleep
+    time_to_sleep
     dice = rand(0..9)
-    if @arr1[dice] == 1
-      puts "Robot with index #{dice} is destroyed."
-      @arr1[dice] = 0
+    if @arr1[dice] > 0
+      damage = rand(30..100)
+      @arr1[dice] -= damage
+      puts "Robot with index #{dice} was hit for #{damage} damage points."
+      time_to_sleep
+      if @arr1[dice] > 0
+        puts "His health is reduced to #{@arr1[dice]} points."
+      else
+        @arr1[dice] = 0
+        puts "Robot with index #{dice} is destroyed."
+      end
     elsif @arr1[dice] == 0
       puts "Miss!"
     end
   
-    random_sleep
+    time_to_sleep
+
+    puts "First team: #{@arr1}."
+
+    time_to_sleep
+
+    puts "Second team: #{@arr2}."
+
+    time_to_sleep
+
+    puts
   
   end
