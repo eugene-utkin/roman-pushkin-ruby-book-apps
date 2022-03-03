bd39296eb03c3affdf6d0566be24ab44d12ebd82
Evgeniy_Utkin <eugene.utkin@gmail.com>
2022-03-03 22:54:05 +0500

    working with arrays examples

---
+++ app53.rb
@@ -0,0 +1,4 @@
+arr = Array.new(10, [])
+element = arr[4]
+element.push('something')
+puts arr.inspect # способ вывести информацию на экран так же, как ее выводит REPL
---
+++ app54.rb
@@ -0,0 +1,4 @@
+arr = Array.new(10, 'something')
+element = arr[4]
+element.upcase!
+puts arr.inspect # способ вывести информацию на экран так же, как ее выводит REPL
